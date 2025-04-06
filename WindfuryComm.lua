78d592004239f73f8d945a1ec9f11f2db0e3ddbe
E1ila <E1ila@users.noreply.github.com>
2025-04-06 14:11:41 +0300

    tracking more totens: STR, AGI, FR, FrR, Grounding

--- WindfuryComm.lua
+++ WindfuryComm.lua
@@ -88,34 +88,6 @@ local function modLayout()
 	end
 end
 
-local function registerWfComm()
-	if wfcLib then
-		wfcLib.UptimeReportHook = function (combatTime, combatUptime, shaman, reporter, channel)
-			if encounterEnded or wfcdb.alwaysReport then
-				local uptime = math.floor(combatUptime / combatTime * 100)
-				local color = "|cffff0000"
-				if uptime > 90 then
-					color = "|cff00ff00"
-				elseif uptime > 80 then
-					color = "|cffb9f542"
-				elseif uptime > 50 then
-					color = "|cfff5ef42"
-				elseif uptime > 25 then
-					color = "|cfff5a142"
-				end
-				if encounterEnded then
-					out('Encounter|cff00ff00', encounterEnded, '|rended with uptime of'..color, tostring(uptime)..'%', '|rby', '|cff0070DE'..shaman)
-				else
-					out('Combat ended with uptime of'..color, tostring(uptime)..'%', '|rby', '|cff0070DE'..shaman)
-				end
-				encounterEnded = nil
-			end
-		end
-	else
-		out("LibWFcomm not found!!")
-	end
-end
-
 local function collectGroupInfo()
 	wfcBgFrame:Show() -- group joined, show frame
 	wipe(wfc.ixs)
@@ -218,6 +190,55 @@ local function restartCurrentTimers()
 	wipe(wfc.currentTimers)
 end
 
+local function uptimePercent(uptime)
+	local color = '|cffff0000'
+	if uptime > 90 then
+		color = '|cff00ff00'
+	elseif uptime > 80 then
+		color = '|cffb9f542'
+	elseif uptime > 60 then
+		color = '|cfff5ef42'
+	elseif uptime > 40 then
+		color = '|cfff5a142'
+	end
+	return color..tostring(uptime)..'%|r'
+end
+
+local function registerWfComm()
+	if wfcLib then
+		wfcLib.UptimeReportHook = function (combatTime, wfTime, shaman, strTime, agiTime, frTime, frrTime, gndTime, reporter, channel)
+			if encounterEnded or wfcdb.alwaysReport then
+				local wfUP = math.floor(wfTime / combatTime * 100)
+				local msg
+				if encounterEnded then
+					msg = 'Encounter |cff00ff00'..encounterEnded..'|r ended with uptime of |cffff8800WF|r:'..uptimePercent(wfUP)
+				else
+					msg = 'Combat ended with uptime of |cffff8800WF|r:'..uptimePercent(wfUP)
+				end
+				if strTime > 0 then
+					msg = msg..' |cffff8800STR|r:'..uptimePercent(math.floor(strTime / combatTime * 100))
+				end
+				if agiTime > 0 then
+					msg = msg..' |cffff8800AGI|r:'..uptimePercent(math.floor(agiTime / combatTime * 100))
+				end
+				if frTime > 0 then
+					msg = msg..' |cffff8800FR|r:'..uptimePercent(math.floor(frTime / combatTime * 100))
+				end
+				if frrTime > 0 then
+					msg = msg..' |cffff8800FrR|r:'..uptimePercent(math.floor(frrTime / combatTime * 100))
+				end
+				if gndTime > 0 then
+					msg = msg..' |cffff8800GND|r:'..uptimePercent(math.floor(gndTime / combatTime * 100))
+				end
+				out(msg..' by |cff0070DE'..shaman)
+				encounterEnded = nil
+			end
+		end
+	else
+		out("LibWFcomm not found!!")
+	end
+end
+
 function wfc:GROUP_ROSTER_UPDATE()
 	if GetNumGroupMembers() == 0 then
 		resetGroup()
@@ -273,7 +294,7 @@ end
 
 local function wfSlashCommands(entry)
 	local arg1, arg2 = strsplit(" ", entry)
-	if arg1 == "orientation" and (arg2 == "horizontal" or arg2 == "vertical") then
+	if pClass == "SHAMAN" and arg1 == "orientation" and (arg2 == "horizontal" or arg2 == "vertical") then
 		if arg2 == "horizontal" then
 			wfcdb.yspace = 0
 			wfcdb.xspace = 1
@@ -283,21 +304,21 @@ local function wfSlashCommands(entry)
 			wfcdb.xspace = 0
 			modLayout()
 		end
-	elseif arg1 == "spacing" and tonumber(arg2) then
+	elseif pClass == "SHAMAN" and arg1 == "spacing" and tonumber(arg2) then
 		wfcdb.space = tonumber(arg2)
 		modLayout()
-	elseif arg1 == "size" and tonumber(arg2) then
+	elseif pClass == "SHAMAN" and arg1 == "size" and tonumber(arg2) then
 		-- If not handled above, display some sort of help message
 		wfcdb.size = tonumber(arg2)
 		modLayout()
-	elseif arg1 == "warn" and tonumber(arg2) then
+	elseif pClass == "SHAMAN" and arg1 == "warn" and tonumber(arg2) then
 		-- If not handled above, display some sort of help message
 		wfcdb.warnsize = tonumber(arg2)
 		modLayout()
 	elseif arg1 == "debug" then
 		wfcdb.debug = not wfcdb.debug
 		out("Debug print is now " .. (wfcdb.debug and "enabled" or "disabled"))
-	elseif arg1 == "ar" then
+	elseif pClass ~= "SHAMAN" and arg1 == "ar" then
 		if wfcdb.alwaysReport then
 			wfcdb.alwaysReport = false
 			out("Will only report Windfury uptime after boss fights")
@@ -313,24 +334,32 @@ local function wfSlashCommands(entry)
 			end
 		end
 	elseif arg1 == "show" then
-		wfcBgFrame:Show()
-		for i = 0, 3 do
-			wfc.buttons[i]:Show()
+		if pClass == "SHAMAN" then
+			wfcBgFrame:Show()
+			for i = 0, 3 do
+				wfc.buttons[i]:Show()
+			end
 		end
 	elseif arg1 == "hide" then
-		wfcBgFrame:Hide()
-		for i = 0, 3 do
-			wfc.buttons[i]:Hide()
+		if pClass == "SHAMAN" then
+			wfcBgFrame:Hide()
+			for i = 0, 3 do
+				wfc.buttons[i]:Hide()
+			end
 		end
 	else
 		out("WindfuryComm++ commands:")
-		out("/wfc orientation <horizontal/vertical>")
-		out("/wfc size <integer> (" .. wfcdb.size .. ")")
-		out("/wfc warn <integer> (" .. wfcdb.warnsize .. ")")
-		out("/wfc spacing <integer> (" .. wfcdb.space .. ")")
-		out("/wfc ver")
-		out("/wfc ar - always report wf uptime, after each fight")
-		out("/wfc <hide/show>")
+		if pClass ~= "SHAMAN" then
+			out("/wfc <hide/show> - show or hide party icons")
+			out("/wfc ar - always report wf uptime, after each fight")
+		end
+		if pClass == "SHAMAN" then
+			out("/wfc orientation <horizontal/vertical> - layout of icons")
+			out("/wfc size <integer> (" .. wfcdb.size .. ") - scale of icons")
+			out("/wfc spacing <integer> (" .. wfcdb.space .. ") - spacing between icons")
+			out("/wfc warn <integer> (" .. wfcdb.warnsize .. ") - size of warning border")
+		end
+		out("/wfc ver - print version")
 	end
 end
 
--- libs/LibWFcomm/LibWFcomm.lua
+++ libs/LibWFcomm/LibWFcomm.lua
@@ -1,11 +1,37 @@
 assert(LibStub, "WindfuryComm requires LibStub")
 
-local major, minor = "LibWFcomm", 3
+local major, minor = "LibWFcomm", 4
 local LibWFcomm = LibStub:NewLibrary(major, minor)
 local CTL = _G.ChatThrottleLib
 local COMM_PREFIX = "WF_STATUS"
 local COMM_PREFIX_CREDIT = "WF_CREDIT"
-local WF_ENCHANT_SPELL_ID = { [564] = 'WF3', [563] = 'WF2', [1783] = 'WF1' }
+local WF_ENCHANTS = { [564] = 'WF3', [563] = 'WF2', [1783] = 'WF1' }
+local TRACKED_AURAS = {
+    [25362] = "STR",
+    [10441] = "STR",
+    [8163] = "STR",
+    [8162] = "STR",
+    [8076] = "STR",
+    [25360] = "AGI",
+    [10626] = "AGI",
+    [8836] = "AGI",
+    [10535] = "FR",
+    [10534] = "FR",
+    [8185] = "FR",
+    [10477] = "FrR",
+    [10476] = "FrR",
+    [8182] = "FrR",
+    [8178] = "GND",
+}
+local TRACKED_AURA_NAMES = {
+    ["STR"] = "Strength",
+    ["AGI"] = "Agility",
+    ["FR"] = "Fire Resistance",
+    ["FrR"] = "Frost Resistance",
+    ["GND"] = "Grounding Totem",
+}
+local TRACKED_AURAS_LIST = { "WF", "STR", "AGI", "FR", "FrR", "GND" }
+
 C_ChatInfo.RegisterAddonMessagePrefix(COMM_PREFIX)
 C_ChatInfo.RegisterAddonMessagePrefix(COMM_PREFIX_CREDIT)
 
@@ -15,156 +41,220 @@ pClass = select(2, UnitClass("player"))
 local lastExpiration
 local hasRefreshed = false
 local myShaman
-local combatStart
-local combatWfStart
-local combatUptime
+local cs = {
+    ["start"] = {
+        ["combat"] = nil,
+        ["WF"] = nil,
+    },
+    ["time"] = {
+        ["WF"] = 0,
+    },
+}
+LibWFCombatStats = cs
 local selfName = UnitName("player")
 
 local function checkCombatStartOrEnd(combat)
-	if not combatStart and combat then
-		-- combat started
-		combatStart = GetTime()
-		combatUptime = 0
-	elseif combatStart and not combat then
-		-- combat ended
-		if combatWfStart then
-			-- combat ended with wf, sum uptime
-			combatUptime = combatUptime + (GetTime() - combatWfStart)
-		end
-		if myShaman and combatUptime > 1 then
-			-- report uptime
-			local combatTime = GetTime() - combatStart
-			local creditmsg = format("%d:%d:%s", math.floor(combatTime + 0.5), math.floor(combatUptime + 0.5), myShaman)
-			CTL:SendAddonMessage("BULK", COMM_PREFIX_CREDIT, creditmsg, 'RAID')
-			if LibWFcomm.UptimeReportHook then
-				LibWFcomm.UptimeReportHook(combatTime, combatUptime, myShaman, selfName, 'DIRECT')
-			end
-		end
-		combatStart = nil
-		combatWfStart = nil
-	end
+    if not cs.start.combat and combat then
+        -- combat started
+        cs.start = {}
+        cs.start.combat = GetTime()
+        cs.time = {}
+        cs.time.WF = 0
+    elseif cs.start.combat and not combat then
+        -- combat ended
+        -- loop TRACKED_AURAS_LIST and sum time
+        for _, spellCategory in ipairs(TRACKED_AURAS_LIST) do
+            if cs.start[spellCategory] then
+                cs.time[spellCategory] = cs.time[spellCategory] + (GetTime() - cs.start[spellCategory])
+                cs.start[spellCategory] = nil
+            end
+        end
+        local combatTime = math.floor(GetTime() - cs.start.combat + 0.5)
+        if myShaman and combatTime > 1 then
+            -- report uptime
+            local wfTime = math.floor((cs.time.WF or 0) + 0.5)
+            local strTime = math.floor((cs.time.STR or 0) + 0.5)
+            local agiTime = math.floor((cs.time.AGI or 0) + 0.5)
+            local frTime = math.floor((cs.time.FR or 0) + 0.5)
+            local frrTime = math.floor((cs.time.FrR or 0) + 0.5)
+            local gndTime = math.floor((cs.time.GND or 0) + 0.5)
+            local creditmsg = format("%d:%d:%s:%d:%d:%d:%d:%d", combatTime, wfTime, myShaman, strTime, agiTime, frTime, frrTime, gndTime)
+            CTL:SendAddonMessage("BULK", COMM_PREFIX_CREDIT, creditmsg, 'RAID')
+            if LibWFcomm.UptimeReportHook then
+                LibWFcomm.UptimeReportHook(combatTime, cs.time.WF, myShaman, strTime, agiTime, frTime, frrTime, gndTime, selfName, 'DIRECT')
+            end
+        end
+        cs.start.combat = nil
+        cs.start.WF = nil
+    end
 end
 
 local function checkCombatWfStart(enchid)
-	local spellName = WF_ENCHANT_SPELL_ID[enchid]
-	if spellName then
-		if combatStart and not combatWfStart then
-			-- combat started with wf, take time
-			combatWfStart = GetTime()
-		end
-	end
+    local spellName = WF_ENCHANTS[enchid]
+    if spellName then
+        if cs.start.combat and not cs.start.WF then
+            -- combat started with wf, take time
+            cs.start.WF = GetTime()
+        end
+    end
 end
 
 local function checkCombatWfDrop()
-	if combatStart and combatWfStart then
-		combatUptime = combatUptime + (GetTime() - combatWfStart)
-		-- wf dropped in combat, sum uptime
-		combatWfStart = nil
-	end
+    if cs.start.combat and cs.start.WF then
+        cs.time.WF = cs.time.WF + (GetTime() - cs.start.WF)
+        -- wf dropped in combat, sum uptime
+        cs.start.WF = nil
+    end
 end
 
--- new message format C_ChatInfo.SendAddonMessage("WF_STATUS", "<guid>:<id>:<expire>:<lagHome>:additional:stuff", "PARTY")
 function windfuryDurationCheck()
-	local msg
-	local _,_,lagHome,_ = GetNetStats()
-	local mh,expiration,_,enchid,_,_,_,_ = GetWeaponEnchantInfo("player")
-	local combat = InCombatLockdown() and "1" or "0"
-	local isdead = UnitIsDeadOrGhost("player") and "1" or "0"
+    local msg
+    local _, _, lagHome, _ = GetNetStats()
+    local mh, expiration, _, enchid, _, _, _, _ = GetWeaponEnchantInfo("player")
+    local combat = InCombatLockdown() and "1" or "0"
+    local isdead = UnitIsDeadOrGhost("player") and "1" or "0"
 
-	checkCombatStartOrEnd(combat == "1");
+    checkCombatStartOrEnd(combat == "1");
 
-	if mh then
-		-- report wf expiration time
-		msg = format("%s:%d:%d:%d:%s:%s:%d", pGUID, enchid, expiration, lagHome, combat, isdead, minor)
-		checkCombatWfStart(enchid)
-		if lastExpiration == nil or expiration > lastExpiration then
-			hasRefreshed = true
-		end
-	else
-		-- report expired wf
-		msg = format("%s:nil:nil:%s:%s:%s:%d", pGUID, lagHome, combat, isdead, minor)
-		checkCombatWfDrop()
-	end
-	lastExpiration = expiration
+    if mh then
+        -- report wf expiration time
+        msg = format("%s:%d:%d:%d:%s:%s:%d", pGUID, enchid, expiration, lagHome, combat, isdead, minor)
+        checkCombatWfStart(enchid)
+        if lastExpiration == nil or expiration > lastExpiration then
+            hasRefreshed = true
+        end
+    else
+        -- report expired wf
+        msg = format("%s:nil:nil:%s:%s:%s:%d", pGUID, lagHome, combat, isdead, minor)
+        checkCombatWfDrop()
+    end
+    lastExpiration = expiration
 
-	if CTL and msg and (lastStatus ~= mh or hasRefreshed) then
-		CTL:SendAddonMessage("BULK", COMM_PREFIX, msg, 'PARTY')
-		lastStatus = mh
-	end
+    if CTL and msg and (lastStatus ~= mh or hasRefreshed) then
+        CTL:SendAddonMessage("BULK", COMM_PREFIX, msg, 'PARTY')
+        lastStatus = mh
+    end
+end
+
+function auraPresenceCheck()
+    -- check for presence of totem auras
+    if cs.start.combat then
+        local seenAuras = {}
+        for i = 1, 40 do
+            local name, _, _, _, _, _, _, _, _, spellId = UnitBuff("player", i)
+            if not name then break end
+
+            local spellCategory = TRACKED_AURAS[spellId]
+            if spellCategory then
+                seenAuras[spellCategory] = true
+                if not cs.start[spellCategory] then
+                    cs.start[spellCategory] = GetTime()
+                    cs.time[spellCategory] = 0
+                end
+            end
+        end
+        -- remove missing auras from combatStats.start
+        for spellCategory, _ in pairs(TRACKED_AURA_NAMES) do
+            if cs.start[spellCategory] and not seenAuras[spellCategory] then
+                cs.time[spellCategory] = (cs.time[spellCategory] or 0) + (GetTime() - cs.start[spellCategory])
+                cs.start[spellCategory] = nil
+            end
+        end
+    end
 end
 
 function checkForShaman()
-	myShaman = nil
-	for index=1,4 do
-		local pstring = "party"..index
-		local gclass = select(2, UnitClass(pstring))
-		if (gclass == "SHAMAN") then
-			myShaman = UnitName(pstring)
-		end
-	end
-	return myShaman
+    myShaman = nil
+    for index = 1, 4 do
+        local pstring = "party" .. index
+        local gclass = select(2, UnitClass(pstring))
+        if (gclass == "SHAMAN") then
+            myShaman = UnitName(pstring)
+        end
+    end
+    return myShaman
 end
-		
 
 function LibWFcomm:PLAYER_LOGIN()
-	self.eventReg:RegisterEvent("GROUP_ROSTER_UPDATE")
-	self:GROUP_ROSTER_UPDATE()
-	print( "WindfuryComm++ sender module loaded" )
+    self.eventReg:RegisterEvent("GROUP_ROSTER_UPDATE")
+    self:GROUP_ROSTER_UPDATE()
+    print("WindfuryComm++ sender module loaded")
 end
 
 function LibWFcomm:GROUP_ROSTER_UPDATE()
-	if( GetNumGroupMembers() ~= 0 and checkForShaman()) then
-		LibWFcomm.eventReg:RegisterUnitEvent("UNIT_INVENTORY_CHANGED", "player")
-		LibWFcomm.eventReg:RegisterEvent("PLAYER_REGEN_DISABLED")
-		LibWFcomm.eventReg:RegisterEvent("PLAYER_REGEN_ENABLED")
-		LibWFcomm.eventReg:RegisterEvent("PLAYER_DEAD")
-		LibWFcomm.eventReg:RegisterEvent("PLAYER_ALIVE")
-		C_Timer.After(0.15, function() windfuryDurationCheck() end)
-	else
-		LibWFcomm.eventReg:UnregisterEvent("UNIT_INVENTORY_CHANGED")
-		LibWFcomm.eventReg:UnregisterEvent("PLAYER_REGEN_DISABLED")
-		LibWFcomm.eventReg:UnregisterEvent("PLAYER_REGEN_ENABLED")
-		LibWFcomm.eventReg:UnregisterEvent("PLAYER_DEAD")
-		LibWFcomm.eventReg:UnregisterEvent("PLAYER_ALIVE")
-	end
+    if (GetNumGroupMembers() ~= 0 and checkForShaman()) then
+        LibWFcomm.eventReg:RegisterUnitEvent("UNIT_INVENTORY_CHANGED", "player")
+        LibWFcomm.eventReg:RegisterEvent("PLAYER_REGEN_DISABLED")
+        LibWFcomm.eventReg:RegisterEvent("PLAYER_REGEN_ENABLED")
+        LibWFcomm.eventReg:RegisterEvent("PLAYER_DEAD")
+        LibWFcomm.eventReg:RegisterEvent("PLAYER_ALIVE")
+        LibWFcomm.eventReg:RegisterEvent("UNIT_AURA")
+        C_Timer.After(0.15, function()
+            windfuryDurationCheck()
+        end)
+    else
+        LibWFcomm.eventReg:UnregisterEvent("UNIT_INVENTORY_CHANGED")
+        LibWFcomm.eventReg:UnregisterEvent("PLAYER_REGEN_DISABLED")
+        LibWFcomm.eventReg:UnregisterEvent("PLAYER_REGEN_ENABLED")
+        LibWFcomm.eventReg:UnregisterEvent("PLAYER_DEAD")
+        LibWFcomm.eventReg:UnregisterEvent("PLAYER_ALIVE")
+        LibWFcomm.eventReg:UnregisterEvent("UNIT_AURA")
+    end
 end
 
 function LibWFcomm:UNIT_INVENTORY_CHANGED()
-	-- This event fires when:
-	-- • You equip or unequip an item.
-	-- • An item in your equipment slots is changed or swapped.
-	-- • Your durability changes, which may affect certain gear-dependent stats.
-	-- • A transmog change is applied (in some cases).
-	-- • A trinket or weapon with charges has a state change.
-	C_Timer.After(0.15, function() windfuryDurationCheck() end)
+    -- This event fires when:
+    -- • You equip or unequip an item.
+    -- • An item in your equipment slots is changed or swapped.
+    -- • Your durability changes, which may affect certain gear-dependent stats.
+    -- • A transmog change is applied (in some cases).
+    -- • A trinket or weapon with charges has a state change.
+    C_Timer.After(0.15, function()
+        windfuryDurationCheck()
+    end)
 end
 
 function LibWFcomm:PLAYER_REGEN_DISABLED()
-	C_Timer.After(0.15, function() windfuryDurationCheck() end)
+    C_Timer.After(0.15, function()
+        windfuryDurationCheck()
+        auraPresenceCheck()
+    end)
 end
 
 function LibWFcomm:PLAYER_REGEN_ENABLED()
-	C_Timer.After(0.15, function() windfuryDurationCheck() end)
+    C_Timer.After(0.15, function()
+        windfuryDurationCheck()
+    end)
 end
 
 function LibWFcomm:PLAYER_DEAD()
-	C_Timer.After(0.15, function() windfuryDurationCheck() end)
+    C_Timer.After(0.15, function()
+        windfuryDurationCheck()
+    end)
 end
 
 function LibWFcomm:PLAYER_ALIVE()
-	C_Timer.After(0.15, function() windfuryDurationCheck() end)
+    C_Timer.After(0.15, function()
+        windfuryDurationCheck()
+    end)
+end
+
+function LibWFcomm:UNIT_AURA()
+    C_Timer.After(0.15, function()
+        auraPresenceCheck()
+    end)
 end
 
 local function OnEvent(self, event, ...)
-	LibWFcomm[event](LibWFcomm, ...)
+    LibWFcomm[event](LibWFcomm, ...)
 end
 
-if ( pClass == "WARRIOR" or pClass == "ROGUE" or pClass == "PALADIN" or pClass == "HUNTER" ) then
-	LibWFcomm.eventReg = LibWFcomm.eventReg or CreateFrame("Frame")
-	LibWFcomm.eventReg:SetScript("OnEvent", OnEvent)
-	if( not IsLoggedIn() ) then
-		LibWFcomm.eventReg:RegisterEvent("PLAYER_LOGIN")
-	else
-		LibWFcomm:PLAYER_LOGIN()
-	end
+if (pClass == "WARRIOR" or pClass == "ROGUE" or pClass == "PALADIN" or pClass == "HUNTER") then
+    LibWFcomm.eventReg = LibWFcomm.eventReg or CreateFrame("Frame")
+    LibWFcomm.eventReg:SetScript("OnEvent", OnEvent)
+    if (not IsLoggedIn()) then
+        LibWFcomm.eventReg:RegisterEvent("PLAYER_LOGIN")
+    else
+        LibWFcomm:PLAYER_LOGIN()
+    end
 end
